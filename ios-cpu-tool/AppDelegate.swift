d093f374d94b4b20964d246fcc7b8159d2598b0d
Vitaly Vidmirov <vvid@mail.ru>
2018-11-01 09:01:06 +0300

    Add state transition logs to AddDelegate
    Show what's happened

--- ios-cpu-tool/AppDelegate.swift
+++ ios-cpu-tool/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import os.log
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -22,23 +23,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        os_log("%{public}@", "applicationWillResignActive()")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        os_log("%{public}@", "applicationDidEnterBackground()")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        os_log("%{public}@", "applicationWillEnterForeground()")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        os_log("%{public}@", "applicationDidBecomeActive()")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        os_log("%{public}@", "applicationWillTerminate()")
     }
 
 
